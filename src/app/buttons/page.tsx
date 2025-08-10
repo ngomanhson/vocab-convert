2e482038aaff87fcd034ca30eb24cf4a7ae1e2df
ngomanhson <ngomanhson2004txpt@gmail.com>
2025-08-10 19:42:11 +0700

    custom component button 2

--- src/app/buttons/page.tsx
+++ src/app/buttons/page.tsx
@@ -3,11 +3,11 @@ import HeartIcon from "@/components/Icons";
 
 export default function ButtonsPage() {
     return (
-        <div className="bg-white flex items-center justify-center gap-5">
+        <div className="h-screen bg-white flex items-center justify-center gap-5">
             <Button size="sm" variant="primary">Button</Button>
             <Button size="md" variant="secondary">Button</Button>
-            <Button leftIcon={<HeartIcon size="sm" />} size="sm" variant="secondary">Button</Button>
             <Button size="lg" variant="ghost">Button</Button>
+            <Button disabled leftIcon={<HeartIcon />} size="lg" variant="ghost">Button</Button>
         </div>
     )
 }
--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -1,22 +1,28 @@
-import React from "react"
+import React, { cloneElement, ComponentProps, HTMLAttributes } from "react"
 import { twMerge } from "tailwind-merge";
 
 type ButtonVariant = "primary" | "secondary" | "ghost";
 type ButtonSize = "sm" | "md" | "lg";
 
-interface ButtonProps {
+export interface ButtonProps extends ComponentProps<"button"> {
     children: React.ReactNode;
     variant: ButtonVariant;
     size: ButtonSize;
-    leftIcon?: React.ReactElement;
-    rightIcon?: React.ReactElement;
+    leftIcon?: React.ReactElement<HTMLAttributes<HTMLElement>, string>;
+    isLoading?: boolean;
 }
 
-export default function Button({ children, variant = "ghost", size = "lg", leftIcon, rightIcon }: ButtonProps) {
+export default function Button({ children, variant = "ghost", size = "lg", leftIcon, className, isLoading = false, ...buttonProps }: ButtonProps) {
     const variantClassNames: Record<ButtonVariant, string> = {
         "primary": "bg-orange-400 text-white",
         "secondary": "bg-black text-white",
         "ghost": "bg-transparent text-black border border-current",
+    }
+
+    const iconVariantClassNames: Record<ButtonVariant, string> = {
+        "primary": "text-white stroke-current",
+        "secondary": "text-white stroke-current",
+        "ghost": "stroke-current",
     }
 
     const sizeClassNames: Record<ButtonSize, string> = {
@@ -25,14 +31,44 @@ export default function Button({ children, variant = "ghost", size = "lg", leftI
         "lg": "text-lg",
     }
 
+    const iconSizeClassNames: Record<ButtonSize, string> = {
+        "sm": "size-4",
+        "md": "size-5",
+        "lg": "size-6",
+    }
+
+    const loadingSizeClassNames: Record<ButtonSize, string> = {
+        "sm": "size-4",
+        "md": "size-5",
+        "lg": "size-6",
+    }
+
+    const { className: leftIconClassName, ...leftIconProps } = leftIcon?.props || {}
+
+    const leftIconClone = leftIcon ? cloneElement(leftIcon, {
+        className: twMerge(iconSizeClassNames[size], iconVariantClassNames[variant]),
+        ...leftIconProps
+    }) : null;
+
+
+    const loading = <div className={twMerge("absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 size-5 border-2 border-current border-t-transparent border-b-transparent rounded-full animate-spin", loadingSizeClassNames[size])}></div>
+
     return (
-        <button className={
-            twMerge("flex items-center gap-2 cursor-pointer py-2 px-4 rounded-md bg-gray-200 font-medium border border-transparent",
-                variantClassNames[variant],
-                sizeClassNames[size],
-            )}>
-            {leftIcon}
-            {children}
+        <button
+            className={
+                twMerge("flex items-center gap-2 relative py-2 px-4 cursor-pointer rounded-md bg-gray-200 font-medium border border-transparent disabled:cursor-not-allowed disabled:pointer-events-none disabled:select-none disabled:opacity-50",
+                    variantClassNames[variant],
+                    sizeClassNames[size],
+                    leftIconClassName,
+                    className,
+                )}
+            {...buttonProps}
+        >
+            {isLoading ? loading :
+                <>
+                    {leftIconClone}
+                    {children}
+                </>}
         </button>
     );
 }
